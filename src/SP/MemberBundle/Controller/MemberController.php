ca238bcefb4029573c74a86f6674bf1e2b178bf0
RomainRJ <r.jerome02@gmail.com>
2015-08-19 21:31:37 +0200

    Bundle MemberBunle: Creation of MemberController

---
+++ src/SP/MemberBundle/Controller/MemberController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class MemberController
+{
+    public function indexAction()
+    {
+        return new Response("Hello World !");
+    }
+}
