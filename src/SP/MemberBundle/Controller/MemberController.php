e9029adc0c3b7e30b6bfecf95cbff2db9ce531ea
RomainRJ <r.jerome02@gmail.com>
2015-08-20 09:52:55 +0200

    MemberDunble: Route creation and function ncreation in MemberController

--- src/SP/MemberBundle/Controller/MemberController.php
+++ src/SP/MemberBundle/Controller/MemberController.php
@@ -8,6 +8,27 @@ class MemberController
 {
     public function indexAction()
     {
-        return new Response("Hello World !");
+        return new Response("Hello Member !");
     }
+
+    public function viewAction($id)
+    {
+        return new Response("consulter fiche Member: ".$id);
+    }
+
+    public function addAction()
+    {
+        return new Response("ajouter Member !");
+    }
+
+    public function updateAction($id)
+    {
+        return new Response("update Member".$id);
+    }
+
+    public function deleteAction($id)
+    {
+        return new Response("Attention!! Delete Member ".$id);
+    }
+
 }
