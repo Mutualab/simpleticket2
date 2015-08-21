b5398fb5bbbddc9211595c18fe477a6860a92308
RomainRJ <r.jerome02@gmail.com>
2015-08-21 13:54:48 +0200

    FOSUserBudnle: Installation and setup of the bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 
             new AppBundle\AppBundle(),
             new SP\MemberBundle\SPMemberBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
