2f3f3e3fe305feedfeb864da540e73a225e0e080
RomainRJ <r.jerome02@gmail.com>
2015-08-27 11:58:44 +0200

    MemberBundle: update entity table name + Controller creation

---
+++ src/SP/MemberBundle/Controller/CompanyController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace SP\MemberBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+// service templating
+use Symfony\Component\HttpFoundation\Request;
+
+use SP\MemberBundle\Entity\Company;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class CompanyController extends Controller
+{
+    public function indexAction()
+    {
+       $listcompamies = $this->getDoctrine()
+        ->getRepository('SPMemberBundle:Company')
+        ->findAll();
+
+
+         return $this->render('SPMemberBundle:Company:index.html.twig',array('listcompamies'=> $listcompamies));
+    }
+
+    public function viewAction($id)
+    {
+        // Ici, on récupérera la fiche correspondante à l'id $id
+        $company = $this->getDoctrine()
+        ->getRepository('SPMemberBundle:Company')
+        ->find($id);
+
+
+         return $this->render('SPMemberBundle:Company:view.html.twig',array('company'=> $company));
+    }
+
+    public function addAction(Request $request)
+    {
+      // Ici, on s'occupe de la création et de la gestion du formulaire
+        $company = new Company();
+
+        $form = $this->createFormBuilder($company)
+            ->add('cpyName', 'text',array('required'=>true, 'max_length'=>64))
+            ->add('save', 'submit')
+            ->getForm();
+
+         $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        // sauvegarde la compagnie dans la bdd
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($company);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('sp_company_view',array('id'=>$company->getId())));
+    }
+        return $this->render('SPMemberBundle:Company:add.html.twig', array(          'form'=>$form->createView(),
+        ));
+
+    }
+
+    public function editAction($id, Request $request)
+    {
+         // Ici, on récupérera la fiche correspondante à $id
+
+
+    // Même mécanisme que pour l'ajout
+
+    if ($request->isMethod('POST')) {$request->getSession()->getFlashBag()->add('notice','Fiche bien modifiée.');
+
+      return $this->redirect($this->generateUrl('sp_company_view', array('id'=>5)));
+
+    }
+
+
+    return $this->render('SPMemberBundle:Company:edit.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+        // Ici, on récupérera la fiche correspondant à $id
+
+    // Ici, on gérera la suppression de la fiche en question
+
+
+    return $this->render('SPMemberBundle:Company:delete.html.twig');
+    }
+
+}
--- src/SP/MemberBundle/Entity/Company.php
+++ src/SP/MemberBundle/Entity/Company.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Company
  *
- * @ORM\Table()
+ * @ORM\Table(name="company")
  * @ORM\Entity(repositoryClass="SP\MemberBundle\Entity\CompanyRepository")
  */
 class Company
